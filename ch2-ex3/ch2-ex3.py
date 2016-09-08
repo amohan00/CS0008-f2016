e5d7e02397985ae9434117fbaa833ac3cdbca2bd
amohan00 <arm198@pitt.edu>
2016-09-08 12:47:28 -0400

    First draft of exercise 2

---
+++ ch2-ex3/ch2-ex3.py
@@ -0,0 +1,4 @@
+# asks the user to enter land in square meters and calculates the number of acres
+# here I am asking the user to input a value for the area of their land and assigning that number to a float type variable
+land = float(input("Enter area of land in square meters. Please use only numbers and decimals"));
+acres = land/4046.8564224;
