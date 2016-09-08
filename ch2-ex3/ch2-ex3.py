e91bc23c854bffa46d3bdabdd13aa7185d6732fc
amohan00 <arm198@pitt.edu>
2016-09-08 13:25:13 -0400

    ch2-ex3
    I added the template

--- ch2-ex3/ch2-ex3.py
+++ ch2-ex3/ch2-ex3.py
@@ -1,8 +1,24 @@
-# asks the user to enter land in square meters and calculates the number of acres
+#
+# Template for code submission
+# name       : Arun Mohan
+# email      : arm198@pitt.edu
+# date       : September 8, 2016
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, Exercise 3
+#
+# Notes: 
+# any notes to the instructor and/or TA goes here
+
+# ...and now let's program with Python
 
 # here I am asking the user to input a value for the area of their land and assigning that number to a variable.
 land = float(input("Enter area of land in square meters. Please use only numbers and decimals: "));
+
 # here I am reassiging the variable to the user's input divided by one acre to find how many acres it is equal to.
 land = land/4046.8564224;
+
 # here I am printing the total number of acres
 print ("There are ", land, " acres in this tract");
