d472d6eb206b2c5184baa4f89810376befadded0
amohan00 <arm198@pitt.edu>
2016-09-15 12:44:24 -0400

    Finished Exercie with comments

--- ch2-ex9/ch2-ex9.py
+++ ch2-ex9/ch2-ex9.py
@@ -1,16 +1,16 @@
 
 
-cels = (int(input("Enter a temperature in Fahrenheit \
+temp = (int(input("Enter a temperature in Fahrenheit \
 or Celsius using only integers: ")));
 temp = (input("Enter 'F' or 'f' if the temperature is in Fahrenheit \
 \nor 'C' or 'c' if the temperature is in Celsius: \n"));
-if temp == "F":
-    convert1 = 5*(cels - 32)/9;
-elif temp == "f":
-    convert1 = 5*(cels - 32)/9;
-elif temp == "C":
+if temp == "F" or temp == "f":
+    convert1 = 5*(cels - 32)/9
+    unit = "Fahrenheit"
+    unit2 = "Celsius"
+else:
     convert1 = (9/5)*cels + 32;
-elif temp == "c":
-    convert1 = (9/5)*cels + 32;
-print ("You entered", cels, "degrees Fahrenheit.\n\
-    That is equal to", convert1, "degrees Celsius");
+    unit = "Celsius"
+    unit2 = "Fahrenheit"
+print ("You entered", cels, "degrees", unit,\
+    "\nThat is equal to", convert1, "degrees", unit2);
