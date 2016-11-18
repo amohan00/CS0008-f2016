63d6863643868e9d89b7b4f8fce39f3bd5898d8f
amohan00 <arm198@pitt.edu>
2016-11-18 15:05:19 -0500

    Assignment 3 started - extracting the file names

--- f2016_cs8_arm198_a3/f2016_cs8_arm198_a3.py
+++ f2016_cs8_arm198_a3/f2016_cs8_arm198_a3.py
@@ -1,4 +1,4 @@
-# Empty List with the file names from the master file
+# Empty List to store the file names from the master file
 file_names = []
 
 # Variable to count the number of files
@@ -7,7 +7,37 @@ file_counter = 0
 # Opening the master file
 master_file = open("f2016_cs8_a3.data.txt", 'r')
 
-# Loop to count the files and add the file names to the list
+# Loop to count the files in the master file and add the file names to the list
 for header in master_file:
     file_counter += 1
     file_names.append(header.rstrip('\n'))
+
+# Closing the master file
+master_file.close()
+
+# Variable to count the number of lines total
+line_counter = 0
+
+# Empty lists to hold all the distances and names
+distances = []
+people = []
+
+# For loop to go through and open each file
+for line in file_names:
+    names = open(line, 'r')
+
+    # For loop to go through each file line by line
+    for row in names:
+        # Splits each line by the comma and assigns the values to a list
+        temp = row.split(',')
+        # Removes the new line character from the distance and adds it to a list
+        distances.append(temp[1].rstrip('\n'))
+        # Removes the space from the names and adds them to a list
+        people.append(temp[0].strip(' '))
+        # Adds one to the line counter
+        line_counter += 1
+    # Closes the file
+    names.close()
+
+print(distances)
+print(people)
